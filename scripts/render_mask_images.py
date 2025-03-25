e3766fe720de2039a6212562acc42eafa43ab568
lzlcs <3012386836@qq.com>
2025-03-25 11:24:17 +0000

    first commit

---
+++ scripts/render_mask_images.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append("./gs")
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+workspace_dir = os.path.dirname(script_dir)
+sys.path.append(workspace_dir)
+
+from abcgs.utils.image import render_depth_or_mask_images
+import torch
+
+pt_path = sys.argv[1]
+masks = torch.load(os.path.join(pt_path, "masks.pt"))
+for i in range(len(masks)):
+    render_depth_or_mask_images(os.path.join(pt_path, f"{i}.jpg"), masks[i])
---
+++ style_transfer.py
@@ -0,0 +1,29 @@
+
+import sys
+sys.path.append("./gs")
+
+import torch
+from abcgs.configs import parse_args
+from gs.utils.general_utils import safe_state
+from gs.gaussian_renderer import network_gui
+from abcgs.trainer import StyleTrainer
+import random
+
+def main():
+    
+    config = parse_args()
+    
+    random.seed(config.style.random_seed)
+    torch.manual_seed(config.style.random_seed)
+    torch.cuda.manual_seed_all(config.style.random_seed)
+    
+    safe_state(config.app.quiet)
+    network_gui.init(config.app.ip, config.app.port)
+    torch.autograd.set_detect_anomaly(config.app.detect_anomaly)
+    
+    trainer = StyleTrainer(config)
+    trainer.train()
+
+if __name__ == "__main__":
+    main()
+
